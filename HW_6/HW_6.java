ad933b81eb730a933735a7cc32cffd560fbfb597
Egor <148006251+EgorGoluzin@users.noreply.github.com>
2023-12-05 21:32:55 +0300

    Create HW_6.java

---
+++ HW_6/HW_6.java
@@ -0,0 +1,23 @@
+package  HW_6;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        StackArray<Integer> stack = new StackArray<Integer>(8);
+
+        System.out.println("Is stack empty: " + stack.isEmpty());
+
+        for (int index = 0; index < 10; index += 1)
+        {
+            stack.push(index);
+        }
+
+        System.out.println("In stack " + stack.getSize() + " elements");
+        System.out.println("Last element: " + stack.peek());
+
+        stack.pop();
+
+        System.out.println("In stack " + stack.getSize() + " elements");
+        System.out.println("Last element: " + stack.peek());
+    };
+}
